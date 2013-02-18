2ae271606cadd3c8c6575b28a790bbbf5b041850
sajingeo <sajin.geo@gmail.com>
2013-02-18 01:12:19 +0000

    multiple links resolved

--- script.py
+++ script.py
@@ -14,14 +14,15 @@ os.system("pacmd set-sink-volume 0 65537")
 #edit wait time, to search for devices 1 unit = 2 sec , 3 would give six seconds
 WAIT=4
 
-while(counter<=WAIT):
+while((counter<=WAIT)and(connected==False)):
 	response=os.popen('pactl list sources short')
 	text=response.read()
 	if(text.find("bluez")<0):
 		print("i am sleeping")
 		time.sleep(2);
 	else:
-# edit this line to add your phone's bluetooth MAC		
+# edit this line to add your phone's bluetooth MAC
+		connected= True		
 		os.system("pactl load-module module-loopback source=bluez_source.50_EA_D6_DE_D9_39 sink=alsa_output.platform-bcm2835_AUD0.0.analog-stereo")
 	        counter=3
 	counter+=1
